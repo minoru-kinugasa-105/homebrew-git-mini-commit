0e3421be71f05e1acdc75282023818ccd6d1e8d8
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2025-10-21 16:53:30 +0900

    Fix SHA256 checksum

--- git-mini-commit.rb
+++ git-mini-commit.rb
@@ -2,13 +2,8 @@ class GitMiniCommit < Formula
   desc "Git CLI extension for mini-commit workflow: local, small commits between staging and regular commit"
   homepage "https://github.com/minoru-kinugasa-105/git-mini-commit"
   url "https://github.com/minoru-kinugasa-105/mini-commit/releases/download/v0.1.1/git-mini-commit"
-  sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
+  sha256 "5c2a3bc8b85331193e97ac0276b1139cac2a2502f8e4008f88273e5e24e97ef3"
   license "MIT"
-
-  if Hardware::CPU.arm?
-    url "https://github.com/minoru-kinugasa-105/mini-commit/releases/download/v0.1.1/git-mini-commit"
-    sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
-  end
 
   def install
     bin.install "git-mini-commit"
