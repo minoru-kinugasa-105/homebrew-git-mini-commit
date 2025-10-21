faa13219db04c605ecf5fdcdedcf7890773779a7
衣笠みのる <168079463+minoru-kinugasa-105@users.noreply.github.com>
2025-10-21 16:47:05 +0900

    Update to v0.1.1

--- git-mini-commit.rb
+++ git-mini-commit.rb
@@ -1,21 +1,17 @@
 class GitMiniCommit < Formula
   desc "Git CLI extension for mini-commit workflow: local, small commits between staging and regular commit"
   homepage "https://github.com/minoru-kinugasa-105/git-mini-commit"
-  url "https://github.com/minoru-kinugasa-105/git-mini-commit/releases/download/v0.1.0/git-mini-commit-darwin-amd64"
+  url "https://github.com/minoru-kinugasa-105/mini-commit/releases/download/v0.1.1/git-mini-commit"
   sha256 "PLACEHOLDER_SHA256_DARWIN_AMD64"
   license "MIT"
 
   if Hardware::CPU.arm?
-    url "https://github.com/minoru-kinugasa-105/git-mini-commit/releases/download/v0.1.0/git-mini-commit-darwin-arm64"
+    url "https://github.com/minoru-kinugasa-105/mini-commit/releases/download/v0.1.1/git-mini-commit"
     sha256 "PLACEHOLDER_SHA256_DARWIN_ARM64"
   end
 
   def install
-    if Hardware::CPU.arm?
-      bin.install "git-mini-commit-darwin-arm64" => "git-mini-commit"
-    else
-      bin.install "git-mini-commit-darwin-amd64" => "git-mini-commit"
-    end
+    bin.install "git-mini-commit"
   end
 
   test do
